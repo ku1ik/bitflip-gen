fae24a3c2db49c9da307a56508d207948a836601
Marcin Kulik <m@ku1ik.com>
2022-05-07 11:39:33 +0200

    Initial version

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+fn flips(input: &str) -> Vec<String> {
+    let masks: [u8; 8] = [1, 2, 4, 8, 16, 32, 64, 128];
+    let mut outputs = vec![];
+
+    for (i, c) in input.char_indices() {
+        for m in masks {
+            let b = c as u8;
+            let b2 = b ^ m;
+            let c2 = b2 as char;
+
+            if c2.is_ascii() {
+                let mut s = String::new();
+                s.push_str(&input[0..i]);
+                s.push(c2);
+                s.push_str(&input[i+1..]);
+                outputs.push(s);
+            }
+        }
+    }
+
+    outputs
+}
+
+fn main() {
+    for d in flips(&env::args().nth(1).unwrap()) {
+        println!("{}", d);
+    }
+}
